03917e22b44ba61e4f3b63284aac205e437de722
John Lindquist <johnlindquist@gmail.com>
2021-09-20 21:19:50 -0600

    add example

--- scripts/example-script.js
+++ scripts/example-script.js
@@ -1,1 +1,10 @@
-console.log(process.env)
+let { GitHub } = await import("@actions/github")
+
+let github = new GitHub(process.env.GITHUB_TOKEN)
+let response = github.repos.uploadReleaseAsset({
+  url: `https://johnlindquist.com/images/logo/john@2x.png`,
+  name: `john.png`,
+  assetPath: `./john.png`,
+})
+
+console.log(`url: ${response.data.browser_download_url}`)
