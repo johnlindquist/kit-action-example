1e0bfb735c14f00925646fdbb3b84f4fe39b40ed
John Lindquist <johnlindquist@gmail.com>
2021-09-20 21:27:03 -0600

    add example

--- scripts/example-script.js
+++ scripts/example-script.js
@@ -1,10 +1,22 @@
-let { GitHub } = await import("@actions/github")
+let { GitHub, context } = await import("@actions/github")
 
 let github = new GitHub(process.env.GITHUB_TOKEN)
-let response = github.repos.uploadReleaseAsset({
-  url: `https://johnlindquist.com/images/logo/john@2x.png`,
+let { owner, repo } = context.repo
+
+let releaseResponse = await github.repos.createRelease({
+  owner,
+  repo,
+  target_commitish: context.sha,
+  tag_name: `some-tag`,
+})
+
+let uploadResponse = github.repos.uploadReleaseAsset({
+  url: releaseResponse.data.upload_url,
   name: `john.png`,
   assetPath: `./john.png`,
+  file: await download(
+    `https://johnlindquist.com/images/logo/john@2x.png`
+  ),
 })
 
 console.log(`url: ${response.data.browser_download_url}`)
