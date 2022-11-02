28b1950a4e2472778279fe510018452066f09ea2
John Lindquist <johnlindquist@gmail.com>
2022-11-02 10:30:03 -0600

    fix(args): use arg in example

--- scripts/example-script.js
+++ scripts/example-script.js
@@ -8,31 +8,26 @@ let github = new Octokit({
   auth: await env("REPO_TOKEN"),
 })
 
+let url = await url("Enter url:")
+
 let dateTag = format(new Date(), "yyyy-MM-dd-HH-mm")
-let releaseResponse = await github.rest.repos.createRelease(
-  {
-    owner,
-    repo,
-    tag_name: dateTag,
-  }
-)
+let releaseResponse = await github.rest.repos.createRelease({
+  owner,
+  repo,
+  tag_name: dateTag,
+})
 
 let headers = {
   "content-type": "image/png",
 }
 
-let uploadResponse =
-  await github.rest.repos.uploadReleaseAsset({
-    headers,
-    owner,
-    repo,
-    release_id: releaseResponse.data.id,
-    name: "john@2x.png",
-    data: await download(
-      `https://johnlindquist.com/images/logo/john@2x.png`
-    ),
-  })
+let uploadResponse = await github.rest.repos.uploadReleaseAsset({
+  headers,
+  owner,
+  repo,
+  release_id: releaseResponse.data.id,
+  name: path.basename(url),
+  data: await download(url),
+})
 
-console.log(
-  `url: ${uploadResponse.data.browser_download_url}`
-)
+console.log(`url: ${uploadResponse.data.browser_download_url}`)
