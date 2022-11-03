06f276afaededb8958b4b0c67815bf9e32823298
John Lindquist <johnlindquist@gmail.com>
2022-11-03 09:54:22 -0600

    fix(update): yml

--- scripts/example-script.js
+++ scripts/example-script.js
@@ -1,6 +1,7 @@
 let { format } = await npm("date-fns")
 let { context } = await npm("@actions/github")
 let { Octokit } = await npm("@octokit/rest")
+let sharp = await npm("sharp")
 
 let { owner, repo } = context.repo
 
@@ -9,6 +10,8 @@ let github = new Octokit({
 })
 
 let url = await arg("Enter url:")
+let width = await arg("Enter width:")
+let height = await arg("Enter height:")
 
 let dateTag = format(new Date(), "yyyy-MM-dd-HH-mm")
 let releaseResponse = await github.rest.repos.createRelease({
@@ -21,13 +24,16 @@ let headers = {
   "content-type": "image/png",
 }
 
+let buffer = await download(url)
+let data = await sharp(buffer).resize(width, height)
+
 let uploadResponse = await github.rest.repos.uploadReleaseAsset({
   headers,
   owner,
   repo,
   release_id: releaseResponse.data.id,
   name: path.basename(url),
-  data: await download(url),
+  data,
 })
 
 console.log(`url: ${uploadResponse.data.browser_download_url}`)
